eb79c283667ba020e0f5431157172e2e59b5df83
wangwenjie <1772755454@qq.com>
2020-04-27 20:29:29 +0800

    更新mogondb代码

--- mongodb/mongo/index.js
+++ mongodb/mongo/index.js
@@ -10,12 +10,20 @@ app.get("/", (req, res) => {
 
 app.get("/api/list", async (req, res) => {
     // 分页查询
-    const { page} = req.query
+    const { page, category, keyword} = req.query
     try {
         const col = mongo.col("fruits")
-        const total = await col.find().count()
+        //构建查询条件
+        const condition = {}
+        if(category){
+            condition.category = category
+        }
+        if (keyword) {
+            condition.name = { $regex: new RegExp(keyword) }
+        }
+        const total = await col.find(condition).count()
         const fruits = await col
-            .find()
+            .find(condition)
             .skip((page - 1) * 5)
             .limit(5)
             .toArray()
