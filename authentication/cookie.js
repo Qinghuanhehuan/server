3bae7afa2326e78350db58bf220d66f1bdf9a47a
wangwenjie <1772755454@qq.com>
2020-04-28 11:29:25 +0800

    添加鉴权代码

---
+++ authentication/cookie.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const session = {}
+http.createServer((req, res) => {
+    const sessionKey = 'sid'
+    if (req.url === '/favicon.ico') {
+        res.end('')
+        return
+    } else {
+        const cookie = req.headers.cookie
+        if (cookie && cookie.indexOf(sessionKey) > -1) {
+            res.end('Come Back')
+            console.log('cookie:', req.headers.cookie)
+            // 简略写法未必具有通用性
+            const pattern = new RegExp(`${sessionKey}=([^;]+);?\s*`)
+            const sid = pattern.exec(cookie)[1]
+            console.log('session:', sid, session, session[sid])
+        } else {
+            const sid = (Math.random() * 9999999).toFixed()
+            // 设置cookie
+            res.setHeader('Set-Cookie', `${sessionKey}=${sid}`)
+            session[sid] = { name: 'laowang' }
+            res.end('hello cookie')
+        }
+    }
+
+}).listen(3000)
